6be86b7bd6cfc58bef9fe5840bc314aa29ce8f5d
murilodamarioo <murilo.gcdamario@gmail.com>
2025-02-25 19:17:02 -0300

    feat: create prisma service

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
+import { PrismaClient } from '@prisma/client'
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    super({
+      log: ['warn', 'error']
+    })
+  }
+
+  onModuleDestroy() {
+    return this.$connect()
+  }
+
+  onModuleInit() {
+    return this.$disconnect()
+  }
+}
