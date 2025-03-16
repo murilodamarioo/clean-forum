8e00dfaac6897afb3621f23205ef1de8dd89db27
murilodamarioo <murilo.gcdamario@gmail.com>
2025-03-16 17:51:38 -0300

    feat: implements generate token

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,20 @@
+import { AuthModule } from './auth/auth.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma/prisma.service';
 import { CreateAccountController } from './controllers/create-account.controller';
 import { envSchema } from './env';
+import { AuthenticateController } from './controllers/authenticate.controller';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    validate: env => envSchema.parse(env),
-    isGlobal: true
-  })],
-  controllers: [CreateAccountController],
+  imports: [
+    ConfigModule.forRoot({
+      validate: env => envSchema.parse(env),
+      isGlobal: true
+    }),
+    AuthModule
+  ],
+  controllers: [CreateAccountController, AuthenticateController],
   providers: [PrismaService],
 })
 export class AppModule {}
