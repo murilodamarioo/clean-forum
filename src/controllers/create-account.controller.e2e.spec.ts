5754facee4764fb0cab8bed3033765993e5e97fa
murilodamarioo <murilo.gcdamario@gmail.com>
2025-03-17 21:42:15 -0300

    feat: implements create account test E2E

--- src/controllers/create-account.controller.e2e.spec.ts
+++ src/controllers/create-account.controller.e2e.spec.ts
@@ -1,3 +1,28 @@
-test('1 plus 1', () => {
-  expect(1+1).toBe(2)
+import { Test } from '@nestjs/testing'
+import { INestApplication } from '@nestjs/common'
+import { AppModule } from '@/app.module'
+import request from 'supertest'
+import { response } from 'express'
+
+describe('create account (E2E)', () => {
+  let app: INestApplication;
+  
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+  
+  test('[POST] /accounts', async () => {
+    const response = await request(app.getHttpServer()).post('/accounts').send({
+      name: 'John Doe',
+      email: 'johndoe@email.com',
+      password: '123456'
+    })
+
+    expect(response.status).toBe(201)
+  })
 })
