4cf5401268006155f1448b6b51a10050fc2f01fa
murilodamarioo <murilo.gcdamario@gmail.com>
2025-03-19 21:59:21 -0300

    feat: Implements cryptography gateways

---
+++ src/domain/forum/application/cryptography/encrypter.ts
@@ -0,0 +1,5 @@
+export abstract class Encrypter {
+
+  abstract encrypt(payload: Record<string, unknown>): Promise<string>
+
+}
--- src/domain/forum/enterprise/entities/student.ts
+++ src/domain/forum/enterprise/entities/student.ts
@@ -3,14 +3,28 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
 interface StudentProps {
   name: string
+  email: string
+  password: string
 }
 
 export class Student extends Entity<StudentProps> {
 
-    static create(props: StudentProps, id?: UniqueEntityID) {
-      const student = new Student(props, id)
-  
-      return student
-    }
+  get name() {
+    return this.props.name
+  }
+
+  get email() {
+    return this.props.email
+  }
+
+  get password() {
+    return this.props.password
+  }
+
+  static create(props: StudentProps, id?: UniqueEntityID) {
+    const student = new Student(props, id)
+
+    return student
+  }
 
 }
