b7198febe8a7dbe381970127b1e8be64c78a359d
murilodamarioo <murilo.gcdamario@gmail.com>
2025-03-25 18:31:01 -0300

    feat: Use factories E2E tests

--- test/factories/make-questions.ts
+++ test/factories/make-questions.ts
@@ -1,7 +1,10 @@
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Question, QuestionProps } from '@/domain/forum/enterprise/entities/question'
+import { PrismaQuestionMapper } from '@/infra/database/prisma/mappers/prisma-question-mapper'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
 
 import { faker } from '@faker-js/faker'
+import { Injectable } from '@nestjs/common'
 
 export function makeQuestion(override: Partial<QuestionProps> = {}, id?: UniqueEntityID) {
   const question = Question.create(
@@ -15,4 +18,19 @@ export function makeQuestion(override: Partial<QuestionProps> = {}, id?: UniqueE
   )
 
   return question
+}
+
+@Injectable()
+export class QuestionFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaQuestion(data: Partial<QuestionProps> = {}): Promise<Question> {
+    const question = makeQuestion(data)
+
+    await this.prisma.question.create({
+      data: PrismaQuestionMapper.toPrisma(question)
+    })
+
+    return question
+  }
 }
