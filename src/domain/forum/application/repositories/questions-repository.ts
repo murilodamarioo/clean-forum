63e2f6e9fef3faea46108eb1b26d5b480cbf0ff3
murilodamarioo <murilo.gcdamario@gmail.com>
2025-04-21 21:06:24 -0300

    feat ✨: Create Question Details class and implements findDetailsBySlug method

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -1,11 +1,14 @@
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { Question } from '../../enterprise/entities/question'
+import { QuestionDetails } from '../../enterprise/entities/value-objects/question-details'
 
 export abstract class QuestionsRepository {
 
   abstract findById(id: string): Promise<Question | null>
 
   abstract findManyRecent(params: PaginationParams): Promise<Question[]>
+
+  abstract findDetailsBySlug(slug: string): Promise<QuestionDetails | null> 
 
   abstract findBySlug(slug: string): Promise<Question | null> 
 
--- src/domain/forum/application/use-cases/get-question-by-slug.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.ts
@@ -3,20 +3,21 @@ import { Question } from '../../enterprise/entities/question'
 import { QuestionsRepository } from '../repositories/questions-repository'
 import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error'
 import { Injectable } from '@nestjs/common'
+import { QuestionDetails } from '../../enterprise/entities/value-objects/question-details'
 
 
 interface GetQuestionBySlugUseCaseRequest {
   slug: string
 }
 
-type GetQuestionBySlugUseCaseResponse = Either<ResourceNotFoundError, { question: Question }>
+type GetQuestionBySlugUseCaseResponse = Either<ResourceNotFoundError, { question: QuestionDetails }>
 
 @Injectable()
 export class GetQuestionBySlugUseCase {
   constructor(private questionsRepository: QuestionsRepository) {}
  
   async execute({ slug }: GetQuestionBySlugUseCaseRequest): Promise<GetQuestionBySlugUseCaseResponse> {
-    const question  = await this.questionsRepository.findBySlug(slug)
+    const question  = await this.questionsRepository.findDetailsBySlug(slug)
 
     if (!question) {
       return left(new ResourceNotFoundError)
