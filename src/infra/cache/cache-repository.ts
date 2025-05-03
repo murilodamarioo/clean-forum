b9ff52393955cadeac567a333e544835386859c8
murilodamarioo <murilo.gcdamario@gmail.com>
2025-05-03 17:19:03 -0300

    feat ✨: create Cache repository

---
+++ src/infra/cache/cache-repository.ts
@@ -0,0 +1,9 @@
+export abstract class CacheRepository {
+
+  abstract set(key: string, value: string): Promise<void>
+
+  abstract get(key: string): Promise<void>
+
+  abstract delete(key: string): Promise<void>
+
+}
