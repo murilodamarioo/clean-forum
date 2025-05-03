03596aec178224744872290e055b3967138ae6ae
murilodamarioo <murilo.gcdamario@gmail.com>
2025-05-03 19:30:07 -0300

    feat ✨: Create Redis Service

---
+++ src/infra/cache/cache.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common'
+import { EnvModule } from '../env/env.module'
+import { EnvService } from '../env/env.service'
+
+@Module({
+  imports: [EnvModule],
+  providers: [EnvService]
+})
+export class CacheModule {
+  
+}
